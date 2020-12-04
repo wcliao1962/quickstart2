43458b9822d9b307675edafc896a6a69d59c7256
wcliao1962 <wcliao1962@gmail.com>
2020-12-04 18:27:41 +0800

    create Task model and build 1:n relationship between User and Task

--- app/Models/User.php
+++ app/Models/User.php
@@ -58,4 +58,13 @@ class User extends Authenticatable
     protected $appends = [
         'profile_photo_url',
     ];
+
+    /**
+     * 取得該使用者的所有任務。
+     */
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
+
 }
